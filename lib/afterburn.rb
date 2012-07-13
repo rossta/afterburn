f669c4b33cc52fa1e0c27c32fe3e61124618762c
Ross Kaffenberger <rosskaff@gmail.com>
2012-07-13 10:05:26 -0400

    autoload modules

--- lib/afterburn.rb
+++ lib/afterburn.rb
@@ -1,48 +1,13 @@
-require 'redis/namespace'
 require "afterburn/engine"
-require "afterburn/helpers"
-require "afterburn/persistence"
-require "afterburn/api_wrapper"
-require "afterburn/board"
-require "afterburn/member"
-require "afterburn/list"
 
 module Afterburn
-  extend self
+  autoload :RedisConnection, "afterburn/redis_connection"
+  autoload :Helpers, "afterburn/helpers"
+  autoload :Persistence, "afterburn/persistence"
+  autoload :ApiWrapper, "afterburn/api_wrapper"
+  autoload :Board, "afterburn/board"
+  autoload :Member, "afterburn/member"
+  autoload :List, "afterburn/list"
 
-  # Accepts:
-  #   1. A 'hostname:port' String
-  #   2. A 'hostname:port:db' String (to select the Redis db)
-  #   3. A 'hostname:port/namespace' String (to set the Redis namespace)
-  #   4. A Redis URL String 'redis://host:port'
-  #   5. An instance of `Redis`, `Redis::Client`, `Redis::DistRedis`,
-  #      or `Redis::Namespace`.
-  def redis=(server)
-    case server
-    when String
-      if server =~ /redis\:\/\//
-        redis = Redis.connect(:url => server, :thread_safe => true)
-      else
-        server, namespace = server.split('/', 2)
-        host, port, db = server.split(':')
-        redis = Redis.new(:host => host, :port => port,
-          :thread_safe => true, :db => db)
-      end
-      namespace ||= :afterburn
-
-      @redis = Redis::Namespace.new(namespace, :redis => redis)
-    when Redis::Namespace
-      @redis = server
-    else
-      @redis = Redis::Namespace.new(:afterburn, :redis => server)
-    end
-  end
-
-  # Returns the current Redis connection. If none has been created, will
-  # create a new one.
-  def redis
-    return @redis if @redis
-    self.redis = Redis.respond_to?(:connect) ? Redis.connect : "localhost:6379"
-    self.redis
-  end
+  extend RedisConnection
 end
