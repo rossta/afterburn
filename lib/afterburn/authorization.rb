addde418e528fe584c0d0ee0045bf66b3f9d3b42
Ross Kaffenberger <rosskaff@gmail.com>
2012-09-19 17:54:57 -0400

    address const warnings for setting trello authorization

--- lib/afterburn/authorization.rb
+++ lib/afterburn/authorization.rb
@@ -1,7 +1,7 @@
 module Afterburn
   class Authorization
     include Trello
-    include Trello::Authorization
+    include Trello::Authorization # provides OAuthPolicy and OAuthCredential
 
     attr_accessor :trello_user_key, :trello_user_secret, :trello_app_token
 
@@ -11,7 +11,7 @@ module Afterburn
 
     def configure
       yield self if block_given?
-      set_oauth_policy
+      ensure_oauth_policy
       OAuthPolicy.consumer_credential = OAuthCredential.new trello_user_key, trello_user_secret
       OAuthPolicy.token = OAuthCredential.new trello_app_token, nil
       Afterburn::Member.add_member(member)
@@ -35,12 +35,29 @@ module Afterburn
 
     private
 
-    def set_oauth_policy
-      Trello::Authorization.const_set :AuthPolicy, OAuthPolicy unless oauth_policy_set?
+    def ensure_oauth_policy
+      remove_auth_policy
+      set_oauth_policy
     end
 
-    def oauth_policy_set?
-      Trello::Authorization::AuthPolicy == OAuthPolicy
+    def remove_auth_policy
+      return if !auth_policy? || oauth_policy?
+      Trello::Authorization.send(:remove_const, :AuthPolicy)
     end
+
+    def set_oauth_policy
+      return if oauth_policy?
+      Trello::Authorization.const_set :AuthPolicy, OAuthPolicy
+    end
+
+    def auth_policy?
+      Trello::Authorization.const_defined?(:AuthPolicy)
+    end
+
+    def oauth_policy?
+      auth_policy? && Trello::Authorization::AuthPolicy == OAuthPolicy
+    end
+
   end
+
 end
